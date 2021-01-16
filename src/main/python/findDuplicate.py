ef7c584b87745d56b9ce6fe19ebb6888f269c804
JavierMG <javier.molina.21016@gmail.com>
2021-01-16 14:08:16 -0600

    New codefrom pythoncentral.io, ok in v3.6.9

--- src/main/python/findDuplicate.py
+++ src/main/python/findDuplicate.py
@@ -1,14 +1,73 @@
-import sys
-import os
+# dupFinder.py - https://www.pythoncentral.io/finding-duplicate-files-with-python/
+import os, sys
 import hashlib
-
-check_path = (lambda filepath, hashes, p = sys.stdout.write:
-        (lambda hash = hashlib.sha1 (file (filepath).read ()).hexdigest ():
-                ((hash in hashes) and (p ('DUPLICATE FILE\n'
-                                          '   %s\n'
-                                          'of %s\n' % (filepath, hashes[hash])))
-                 or hashes.setdefault (hash, filepath)))())
-
-scan = (lambda dirpath, hashes = {}: 
-                map (lambda (root, dirs, files):
-                        map (lambda filename: check_path (os.path.join (root, filename), hashes), files), os.walk (dirpath)))
+ 
+def findDup(parentFolder):
+    # Dups in format {hash:[names]}
+    dups = {}
+    for dirName, subdirs, fileList in os.walk(parentFolder):
+        print('Scanning %s...' % dirName)
+        for filename in fileList:
+            # Get the path to the file
+            path = os.path.join(dirName, filename)
+            # Calculate hash
+            file_hash = hashfile(path)
+            # Add or append the file path
+            if file_hash in dups:
+                dups[file_hash].append(path)
+            else:
+                dups[file_hash] = [path]
+    return dups
+ 
+ 
+# Joins two dictionaries
+def joinDicts(dict1, dict2):
+    for key in dict2.keys():
+        if key in dict1:
+            dict1[key] = dict1[key] + dict2[key]
+        else:
+            dict1[key] = dict2[key]
+ 
+ 
+def hashfile(path, blocksize = 65536):
+    afile = open(path, 'rb')
+    hasher = hashlib.md5()
+    buf = afile.read(blocksize)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(blocksize)
+    afile.close()
+    return hasher.hexdigest()
+ 
+ 
+def printResults(dict1):
+    results = list(filter(lambda x: len(x) > 1, dict1.values()))
+    if len(results) > 0:
+        print('Duplicates Found:')
+        print('The following files are identical. The name could differ, but the content is identical')
+        print('___________________')
+        for result in results:
+            for subresult in result:
+                print('\t\t%s' % subresult)
+            print('___________________')
+ 
+    else:
+        print('No duplicate files found.')
+ 
+ 
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        dups = {}
+        folders = sys.argv[1:]
+        for i in folders:
+            print("Check for folder", i)
+            # Iterate the folders given
+            if os.path.exists(i):
+                # Find the duplicated files and append them to the dups
+                joinDicts(dups, findDup(i))
+            else:
+                print('%s is not a valid path, please verify' % i)
+                sys.exit()
+        printResults(dups)
+    else:
+        print('Usage: python dupFinder.py folder or python dupFinder.py folder1 folder2 folder3')
